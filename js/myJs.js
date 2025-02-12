7b0f6431f180de4c5c0c1da637ce270496507bc9
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-02-12 16:27:50 +0330

    improve Style and JS

--- js/myJs.js
+++ js/myJs.js
@@ -1,13 +1,40 @@
+//Select Shapes
+const selectShape = document.querySelectorAll(".liShapes");
+// =======================
 const canvas = document.querySelector("canvas"),
 ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+let brushWidth = 5;
+
 
 window.addEventListener("load",()=>{
     canvas.width=canvas.offsetWidth;
     canvas.height=canvas.offsetHeight;
 });
 
+const startDrawing = ()=>{
+    isDrawing = true;
+    console.log("Mouse Down");
+
+    ctx.beginPath();
+    ctx.lineWidth = brushWidth;
+}
+
 const drawing = (e)=>{
+    if(!isDrawing) return;
     ctx.lineTo(e.offsetX, e.offsetY);//creating Line(by Mouse)
     ctx.stroke(); //fill with color
 };
+
+selectShape.forEach(myShape => {
+    myShape.addEventListener("click",()=>{
+        document.querySelector(".ulShapes .active").classList.remove("active");
+        myShape.classList.add("active");
+        console.log(myShape.id);
+    });
+});
+
+canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mousemove", drawing);
+canvas.addEventListener("mouseup", ()=>isDrawing = false);
