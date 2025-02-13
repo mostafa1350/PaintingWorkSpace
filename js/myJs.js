c33765922a01f99480b0d993d74b35994000acfb
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2025-02-14 00:06:19 +0330

    write Rect and Circle Method and Improve Style
    write Rect and Circle Method and Improve Style

--- js/myJs.js
+++ js/myJs.js
@@ -1,37 +1,86 @@
-//Select Shapes
-const selectShape = document.querySelectorAll(".liShapes");
+
 // =======================
 const canvas = document.querySelector("canvas"),
 ctx = canvas.getContext("2d");
+//Select Shapes
+const toolBtns = document.querySelectorAll(".liTool"),
+fillCollor = document.querySelector("#fill-color");
+// ========================
 
-let isDrawing = false;
-let brushWidth = 5;
+let isDrawing = false,
+    selectedTool = "brush",
+    brushWidth = 5;
+
+// =========== Status of X and Y by Mouse ===============
+let pointMouseX , pointMouseY, snapShot;
+// ======================================================
 
 
 window.addEventListener("load",()=>{
     canvas.width=canvas.offsetWidth;
     canvas.height=canvas.offsetHeight;
+    
 });
 
-const startDrawing = ()=>{
+// ============= CREATE RECTANGLE ================
+const drawRectangle = (e)=>{
+    if(!fillCollor.checked)
+    {
+        return ctx.strokeRect(e.offsetX,e.offsetY,pointMouseX - e.offsetX ,pointMouseY - e.offsetY);
+
+    }
+    ctx.fillRect(e.offsetX,e.offsetY,pointMouseX - e.offsetX ,pointMouseY - e.offsetY);
+    
+}
+const drawCircle = (e)=>{
+    ctx.beginPath();
+    let radius = Math.sqrt(Math.pow((pointMouseX-e.offsetX),2) + Math.pow((pointMouseY-e.offsetY),2));
+    // if(!fillCollor.checked)
+    // {
+        ctx.arc(pointMouseX ,pointMouseY, radius, 0, 2*Math.PI);
+        ctx.stroke();
+
+    // }
+    // ctx.fillRect(e.offsetX,e.offsetY,pointMouseX - e.offsetX ,pointMouseY - e.offsetY);
+    
+}
+// ===============================================
+
+const startDrawing = (e)=>{
     isDrawing = true;
     console.log("Mouse Down");
 
+    
+    pointMouseX = e.offsetX;
+    pointMouseY = e.offsetY;
+
     ctx.beginPath();
     ctx.lineWidth = brushWidth;
+
+    snapShot = ctx.getImageData(0, 0, canvas.width, canvas.height )
 }
 
 const drawing = (e)=>{
     if(!isDrawing) return;
-    ctx.lineTo(e.offsetX, e.offsetY);//creating Line(by Mouse)
-    ctx.stroke(); //fill with color
+    ctx.putImageData(snapShot, 0, 0);
+
+    if(selectedTool === "brush"){
+        ctx.lineTo(e.offsetX, e.offsetY);//creating Line(by Mouse)
+        ctx.stroke(); //fill with color
+    }else if(selectedTool === "rectangle"){
+        drawRectangle(e);
+    }else if(selectedTool === "circle"){
+        drawCircle(e);
+    }
+
 };
 
-selectShape.forEach(myShape => {
-    myShape.addEventListener("click",()=>{
+toolBtns.forEach(btn => {
+    btn.addEventListener("click",()=>{
         document.querySelector(".ulShapes .active").classList.remove("active");
-        myShape.classList.add("active");
-        console.log(myShape.id);
+        btn.classList.add("active");
+        selectedTool = btn.id;
+        console.log(selectedTool);
     });
 });
 
